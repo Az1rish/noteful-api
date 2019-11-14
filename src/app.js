ededb94e4dc25799e4fb9d35725369c7ef6cec00
Shane <shanemcneil19@gmail.com>
2019-11-14 07:12:45 -0700

    connect routers to app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const notesRouter = require('./notes/notes-router')
+const folderRouter = require('./folders/folders-router')
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -14,6 +15,9 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use('/api/notes', notesRouter)
+app.use('/api/folders', folderRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, Noteful!')
