c71732f9060aa2cc0749c9a4f598299c6c04f5ed
Shane <shanemcneil19@gmail.com>
2019-11-17 13:38:27 +0000

    create foldersRouter and test file, connect to app, add test for case of GET with no folders in db, passing

---
+++ src/folders/folders-router.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const FoldersService = require('./folders-service')
+const foldersRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeFolder = folder => ({
+    id: folder.id,
+    title: xss(folder.title),
+    date_created: folder.date_created,
+})
+
+foldersRouter
+    .route('/')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        FoldersService.getAllFolders(knexInstance)
+            .then(folders => {
+                res.json(folders.map(serializeFolder))
+            })
+            .catch(next)
+    })
+
+
+module.exports = foldersRouter
